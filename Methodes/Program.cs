c4c2b36bc59a58ad7fb88a03d21cb710f9f417fc
frc <fcaumartin@gmail.com>
2016-08-23 11:14:41 +0200

    Ajout fonction fact

--- Methodes/Program.cs
+++ Methodes/Program.cs
@@ -8,8 +8,20 @@ namespace Methodes
 {
     class Program
     {
+        static int fact (int nb)
+        {
+            int resultat = 1;
+            for (int i = 1; i <= nb; i++)
+            {
+                resultat = resultat * i;
+            }
+            return resultat;
+        }
+
         static void Main(string[] args)
         {
+            int r = fact(4);
+            Console.WriteLine(r);
         }
     }
 }
